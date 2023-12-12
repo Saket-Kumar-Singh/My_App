87f6e02068ee3d69b296308ac13e46fa4c7634b0
Saket-Kumar-Singh <saketkrsingh37772@gmail.com>
2023-12-12 15:34:21 +0530

    Adds authentication and jwt token generation

--- app/resources.py
+++ app/resources.py
@@ -1,3 +1,5 @@
+import datetime
+from datetime import timedelta
 from tkinter import BASELINE
 from typing import Optional
 from fastapi import Body, FastAPI, Request, status, HTTPException, APIRouter
@@ -7,3 +9,4 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from . import models
 import time
+from fastapi.security import OAuth2PasswordBearer
--- app/routers/login.py
+++ app/routers/login.py
@@ -1,6 +1,7 @@
 from .. import utlis
-from ..resources import status, models, HTTPException, APIRouter
+from ..resources import status, models, HTTPException, APIRouter, timedelta
 from ..main import conn, cursor
+from .oauth import create_user_token
 
 router = APIRouter()
 
@@ -10,7 +11,11 @@ def login_user(payload : models.userLogin):
     s = f"select user_password from user_data where user_name = '{payload.user_name}'"
     cursor.execute(s)
     p = cursor.fetchone()
+    if(p is None):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect Credentials")
     if(utlis.match(pload['user_password'], p['user_password'])):
-        return {"details" : "Correct_Credentials"}
+        token = create_user_token(17, payload.user_name, timedelta(minutes = 20))
+        return {"token" : token}
     else:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail= "Incorrect Credentials")
+    
