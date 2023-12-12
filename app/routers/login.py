10ff2fee5e5ffa2c31f1a520917c376973629b35
Saket-Kumar-Singh <saketkrsingh37772@gmail.com>
2023-12-12 21:29:44 +0530

    Corrects login authentication

--- app/routers/login.py
+++ app/routers/login.py
@@ -2,20 +2,28 @@ from .. import utlis
 from ..resources import status, models, HTTPException, APIRouter, timedelta
 from ..main import conn, cursor
 from .oauth import create_user_token
+from passlib.context import CryptContext
+from jose import JWTError
 
-router = APIRouter()
+router = APIRouter(
+    prefix = '/login',
+    tags = ['login']
+)
 
-@router.post("/login", status_code=status.HTTP_202_ACCEPTED)
+@router.post("/", status_code=status.HTTP_202_ACCEPTED)
 def login_user(payload : models.userLogin):
-    pload = payload.dict()
-    s = f"select user_password from user_data where user_name = '{payload.user_name}'"
-    cursor.execute(s)
-    p = cursor.fetchone()
-    if(p is None):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect Credentials")
-    if(utlis.match(pload['user_password'], p['user_password'])):
-        token = create_user_token(17, payload.user_name, timedelta(minutes = 20))
-        return {"token" : token}
-    else:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail= "Incorrect Credentials")
+    try:
+        pload = payload.dict()
+        s = f"select * from user_data where user_name = '{payload.user_name}'"
+        cursor.execute(s)
+        p = cursor.fetchone()
+        if(p is None):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect Credentials")
+        if(utlis.match(pload['user_password'], p['user_password'])):
+            token = create_user_token(p['user_id'], payload.user_name, timedelta(minutes = 20))
+            return {"token" : token}
+        else:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail= "Incorrect Credentials")
+    except:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail = "Something's off")    
     
--- app/routers/oauth.py
+++ app/routers/oauth.py
@@ -1,9 +1,9 @@
 from ..models import userOut
 from ..main import APIRouter 
 from ..utlis import SECRET_KEY, ALGORITHM, CryptContext
-from ..resources import OAuth2PasswordBearer
+from ..resources import OAuth2PasswordBearer, Depends
 import datetime
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.hash import bcrypt
 
 router = APIRouter(
@@ -11,10 +11,11 @@ router = APIRouter(
     tags = ['oath']
 )
 
-# bycrypt_context = CryptContext(schemes = [bcrypt], depecreted = "auto")
+pwd_context = CryptContext(schemes = ['bcrypt'], deprecated = "auto")
 oauth_bearer = OAuth2PasswordBearer(tokenUrl = "oauth/token")
 
 def create_user_token(user_id, user_name, expiry_time):
  expiry =datetime.datetime.now(datetime.timezone.utc)
  encode = {"id" : user_id, "sub" : user_name, "exp" : expiry}
  return jwt.encode(encode, SECRET_KEY, algorithm =  ALGORITHM )   
+
